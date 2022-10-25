74252927d6f7228cea9974f18dbf2db43b45cc09
Mayara A. Moraes <salanderaraujo@gmail.com>
2022-10-25 15:42:26 -0300

    Adiciona requisito 14

---
+++ app/backend/src/database/models/teams.model.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Teams;
